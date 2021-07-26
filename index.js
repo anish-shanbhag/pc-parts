de7db6fd3239fd2cc78a0f04788c394c3ba86f60
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2021-07-26 04:26:49 -0700

    Clean CPU data

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-(async () => {
+async function writeTemp(data) {
+  fs.writeFileSync("./temp.json", JSON.stringify(data, null, 2))
+}
+
+async function scrapeCPUs () {
   const browser = await puppeteer.launch({
     headless: false,
     args: [`--window-size=1680,820`],
@@ -72,6 +76,55 @@ const fs = require("fs");
   for (let i = 0; i < 12; i++) {
     getCPULink();
   }
-})();
+}
 
-"li[style*=\"border: solid red\"]"
+async function cleanCPUs() {
+  let cpus = require("./data/cpu.json");
+  cpus = cpus.sort((a, b) => a.name.localeCompare(b.name));
+  for (const cpu of cpus) {
+    const updatedBenchmarks = {};
+    for (const key in cpu) {
+      if (key != "name" && key != "stats") {
+        const newKey = key.toLowerCase().replaceAll(" ", "_");
+        const value = parseFloat(cpu[key].match(/[\d\.,]+/)[0].replaceAll(",", ""));
+        delete cpu[key];
+        updatedBenchmarks[newKey] = value;
+      }
+    }
+    if (!cpu.stats[0].includes(":")) {
+      cpu.stats.shift();
+    }
+    const statsMap = {
+      "Class:  (\\w+)": "class",
+      "Socket: (.+) Clockspeed": "socket",
+      "Clockspeed": ["base_clock"],
+      "Turbo Speed": ["turbo_clock"],
+      "Cores": ["cores"],
+      "Typical TDP": ["tdp"],
+      "CPU First Seen on Charts:\\s+(Q.{6})": "release",
+      "Overall Rank": ["overall_rank"],
+      "\\t(\\d+)": ["cpu_mark_rating"],
+      "Single Thread Rating": ["single_thread_rating"],
+      "Cross-Platform Rating": ["cross_platform_rating"],
+      "Samples": ["samples"],
+      "CPU Mark": ["old_cpu_mark_rating"],
+      "Thread": ["old_single_thread_rating"]
+    }
+    for (const stat in statsMap) {
+      for (const string of cpu.stats) {
+        let regex = stat.includes("(") ? stat : stat + ":\\s+([\\d\\.,]+)";
+        const match = string.match(new RegExp(regex));
+        if (match) {
+          let value = match[1].trim();
+          if (Array.isArray(statsMap[stat])) value = parseFloat(value.replaceAll(",", ""));
+          cpu[statsMap[stat]] = value;
+        }
+      }
+    }
+    delete cpu["stats"];
+    Object.assign(cpu, updatedBenchmarks);
+  }
+  fs.writeFileSync("./data/cpu_cleaned.json", JSON.stringify(cpus, null, 2))
+}
+
+cleanCPUs();
